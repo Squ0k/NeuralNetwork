41dcf3c4cdf36e8d6da0c61144d21faebf3a33e3
Squ0k <159511824+Squ0k@users.noreply.github.com>
2026-02-19 22:02:06 -0800

    Main has a demo now

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-import Source.NNRegression;
+import Source.*;
 
 public class Main {
     public static void main(String[] args) {
-        NNClassifier reg = new NNClassifier()
+        NNRegression model = new NNRegression(new int[]{1}, 2);
+        for (int i=0;i<10000;i++) {
+            double a = Math.random() * 10;
+            double b = Math.random() * 10;
+
+            model.train(new double[]{a, b}, a+b, 0.001);
+        }
+        for (int i=0;i<10;i++) {
+            double a = (int) (Math.random() * 10);
+            double b = (int) (Math.random() * 10);
+
+            double c = model.predict(new double[]{a, b});
+            System.out.println(a + " + " + b + " = " + c);
+        }
     }
 }
